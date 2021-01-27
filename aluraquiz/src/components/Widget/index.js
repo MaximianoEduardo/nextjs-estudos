1506ff5196ea19f368a7938da7b5695601a15c72
Eduardo Maximiano <edu.maxi.96@gmail.com>
2021-01-27 16:41:18 -0300

    fix: Correções encontradas pelo eslint :no_pedestrians:

--- aluraquiz/src/components/Widget/index.js
+++ aluraquiz/src/components/Widget/index.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const Widget = styled.div`
     margin: 24px 0;
@@ -45,6 +45,45 @@ Widget.Content = styled.div`
         list-style: none;
         padding: 0;
     }
+
+	form{
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+
+		input{
+			height: 36px;
+			padding-left: 15px;
+			border: 1px solid ${({ theme }) => theme.colors.contrastText};
+			border-radius: 3.5px;
+			background: transparent;
+			color: ${({ theme }) => theme.colors.contrastText};
+			font-weight: 400;
+			font-size: 14px;
+			letter-spacing: 0.15px;
+		}
+
+		button{
+			margin-top: 25px;
+			height: 36px;
+			border: none;
+			border-radius: ${({ theme }) => theme.borderRadius};
+			background-color: ${({ theme }) => theme.colors.primary};
+			font-weight: 700;
+			color: ${({ theme }) => theme.colors.contrastText};
+			font-size: 14px;
+			letter-spacing: 1.25px;
+			transition: 300ms ease-in;
+		}
+
+		button:disabled,
+		button[disabled]{
+			background: #979797;
+		}
+
+	}
+
+    
 `;
 
 export default Widget;
