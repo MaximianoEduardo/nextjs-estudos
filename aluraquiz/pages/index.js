e86927b1121ff3ed3be5c1cc73be09fd725a18ee
Eduardo Maximiano <edu.maxi.96@gmail.com>
2021-01-27 16:42:21 -0300

    feat: usando State

--- aluraquiz/pages/index.js
+++ aluraquiz/pages/index.js
@@ -1,9 +1,12 @@
-import db from "../db.json";
-import styled from "styled-components";
-import QuizBackground from "../src/components/QuizBackground";
-import Widget from "../src/components/Widget";
-import Footer from "../src/components/Footer";
-import GitHubCorner from "../src/components/GitHubCorner";
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { useRouter } from 'next/router';
+import db from '../db.json';
+import QuizBackground from '../src/components/QuizBackground';
+import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizLogo from '../src/components/QuizLogo';
 
 const QuizContainer = styled.div`
     width: 100%;
@@ -17,26 +20,45 @@ const QuizContainer = styled.div`
 `;
 
 export default function Home() {
-    return (
-        <QuizBackground backgroundImage={db.bg}>
-            <QuizContainer>
-                <Widget>
-                    <Widget.Header>
-                        <h1> The legend of zelda </h1>
-                    </Widget.Header>
-                    <Widget.Content>
-                        <p> Lorem ipsum </p>
-                    </Widget.Content>
-                </Widget>
-                <Widget>
-                    <Widget.Content>
-                        <h1> The legend of zelda </h1>
-                        <p> Lorem ipsum </p>
-                    </Widget.Content>
-                </Widget>
-                <Footer />
-            </QuizContainer>
-            <GitHubCorner projectUrl="https://github.com/MaximianoEduardo" />
-        </QuizBackground>
-    );
+  const router = useRouter();
+
+  const [name, setName] = useState('');
+
+  return (
+    <>
+      <QuizBackground backgroundImage={db.bg}>
+        <QuizContainer>
+          <QuizLogo />
+          <Widget>
+            <Widget.Header>
+              <h1>
+                {db.title}
+              </h1>
+            </Widget.Header>
+            <Widget.Content>
+              <form onSubmit={(e) => {
+                e.preventDefault();
+
+                router.push(`/quiz?name=${name}`);
+              }}
+              >
+                <input
+                  placeholder="Diz ai seu nome pra jogar :)"
+                  onChange={(e) => {
+                    setName(e.target.value);
+                  }}
+                />
+                <button type="submit" disabled={name.length === 0}>
+                  Jogar {''}
+                  { name}
+                </button>
+              </form>
+            </Widget.Content>
+          </Widget>
+          <Footer />
+        </QuizContainer>
+        <GitHubCorner projectUrl="https://github.com/MaximianoEduardo" />
+      </QuizBackground>
+    </>
+  );
 }
