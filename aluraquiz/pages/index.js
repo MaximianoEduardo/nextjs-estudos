a07d6d0dcba520dd49a63443e7fb14126e4eb7c0
Eduardo Maximiano <edu.maxi.96@gmail.com>
2021-02-01 10:54:46 -0300

    refactor: Utilizando novos components :recycle:

--- aluraquiz/pages/index.js
+++ aluraquiz/pages/index.js
@@ -3,22 +3,13 @@ import styled from 'styled-components';
 import { useRouter } from 'next/router';
 import db from '../db.json';
 import QuizBackground from '../src/components/QuizBackground';
+import QuizContainer from '../src/components/QuizContainer'
 import Widget from '../src/components/Widget';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 import QuizLogo from '../src/components/QuizLogo';
 import InputBase from '../src/components/InputBase'
-
-const QuizContainer = styled.div`
-    width: 100%;
-    max-width: 350px;
-    padding-top: 45px;
-    margin: auto 10%;
-    @media screen and (max-width: 500px) {
-        margin: auto;
-        padding: 15px;
-    }
-`;
+import Button from '../src/components/Button';
 
 export default function Home() {
   const router = useRouter();
@@ -45,11 +36,8 @@ export default function Home() {
               >
 				<InputBase onChange={(e) => {
                     setName(e.target.value);
-                  }} placeholder="Diz ai seu nome pra jogar :)" />
-                <button type="submit" disabled={name.length === 0}>
-                  Jogar {''}
-                  { name}
-                </button>
+                  }} placeholder="Diz ai seu nome pra jogar :)" name="nomeDoUsuario" value={name} />
+				<Button type='submit' disabled={name.length === 0}> {`Jogar ${name} `} </Button>
               </form>
             </Widget.Content>
           </Widget>
